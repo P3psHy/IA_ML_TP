5a13b055fb026d0a330c5d1ecd2c4769b0d0d2a4
P3psHy <76128319+P3psHy@users.noreply.github.com>
2025-02-11 11:12:22 +0100

    Ajouts

--- main_01.py
+++ main_01.py
@@ -1,28 +1,139 @@
-import os
-import time
-import urllib.request
-import json
+import os, boto3
+import cv2
 
-def get_text_from_speech(filename, aws_service, job_name, bucket_name):
+from dotenv import load_dotenv
+from matplotlib import pyplot as plt
+
+
+
+def check_filetype(filename):
     """
-    Convertit de la parole en texte en utilisant AWS Transcribe.
+    Détermine le type de fichier en fonction de son extension.
 
-    Cette fonction téléverse un fichier audio spécifié dans un seau S3, démarre un travail de transcription avec AWS Transcribe,
-    attend que le travail soit terminé, et récupère le texte transcrit.
+    Cette fonction prend un nom de fichier en entrée, extrait son extension et détermine
+    le type de fichier (par exemple, image, vidéo). Si l'extension du fichier est reconnue comme un format
+    d'image courant (jpg, png, tiff, svg) ou un format de vidéo courant (mp4, avi, mkv), elle attribue
+    le type correspondant. Sinon, le type de fichier est défini sur None.
 
     Paramètres :
-    - filename (str) : Chemin local vers le fichier audio à transcrire.
-    - aws_service (object) : Client AWS Transcribe configuré.
-    - job_name (str) : Nom unique pour le travail de transcription.
-    - bucket_name (str) : Nom du seau S3 où le fichier audio est stocké.
+    - filename (str) : Le chemin vers le fichier incluant le nom de fichier.
 
     Retourne :
-    - str : Le texte transcrit du fichier audio.
+    - str ou None : Le type de fichier déterminé ('image', 'vidéo') ou None si le type de fichier
+      n'est pas reconnu.
 
-    Prérequis :
-    - Le fichier audio doit déjà être téléversé dans le seau S3 spécifié.
+    Exemple :
+    >>> check_filetype("/chemin/vers/image.jpg")
+    'image'
+    >>> check_filetype("/chemin/vers/video.mp4")
+    'vidéo'
+    >>> check_filetype("/chemin/vers/fichierinconnu.xyz")
+    None
     """
+
+    # Extrait le nom de base du fichier à partir du chemin de fichier fourni.
+    file_basename = os.path.basename(filename)
+
+    # Sépare le nom de base sur le point et prend la dernière partie comme extension.
+    extension = file_basename.split(".")[-1]
+
+    # Détermine le type de fichier en fonction de l'extension.
+    if extension in ["jpg", "png", "tiff", "svg"]:
+        filetype = "image"
+    elif extension in ["mp4", "avi", "mkv"]:
+        filetype = "vidéo"
+    else:
+        filetype = None
+
+    # Enregistre le type de fichier détecté.
+    print(f"[INFO] : Le fichier {file_basename} est de type : {filetype}")
+    
+    return filetype
+
+
+def extract_frame_video(video_path, frame_id):
+    """
+    Extrait une image spécifique d'une vidéo.
+
+    Cette fonction utilise OpenCV pour ouvrir une vidéo à partir du chemin spécifié et extrait une image
+    particulière en fonction de son ID. L'ID de l'image correspond à l'ordre de l'image dans la vidéo, en commençant
+    par 0 pour la première image. Si l'extraction réussit, l'image est retournée sous forme d'un tableau Numpy.
+
+    Paramètres :
+    - video_path (str) : Le chemin vers le fichier vidéo d'où extraire l'image.
+    - frame_id (int) : L'identifiant (ID) de l'image à extraire.
+
+    Retourne :
+    - ndarray ou None : L'image extraite (un tableau Numpy) si l'extraction est réussie,
+      sinon `None`.
+
+    Exemple :
+    >>> image = extract_frame_video("/chemin/vers/video.mp4", 150)
+    >>> type(image)
+    <class 'numpy.ndarray'>
+    """
+
+    # Ouvre la vidéo à partir du chemin fourni.
+    video = cv2.VideoCapture(video_path)
+
+    # Positionne le lecteur vidéo sur l'image spécifiée par frame_id.
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+
+    # Lit l'image actuelle.
+    ret, image = video.read()
+
+    # Si la lecture réussit (ret est True), retourne l'image.
+    # Sinon, retourne None.
+    return image if ret else None
+
+def get_aws_session():
+    """
+    Crée et retourne une session AWS.
+
+    Cette fonction charge les variables d'environnement depuis un fichier .env situé dans le répertoire
+    courant ou les parents de celui-ci, récupère les clés d'accès AWS (`ACCESS_KEY` et `SECRET_KEY`),
+    et initialise une session AWS avec ces identifiants ainsi qu'avec une région spécifiée (dans cet exemple,
+    'us-east-1'). Elle est particulièrement utile pour configurer une session AWS de manière sécurisée sans
+    hardcoder les clés d'accès dans le code.
+
+    Retourne :
+    - Session : Un objet session de boto3 configuré avec les clés d'accès et la région AWS.
+
+    Exemple d'utilisation :
+    >>> session_aws = get_aws_session()
+    >>> type(session_aws)
+    <class 'boto3.session.Session'>
+    """
+
+    # Charge les variables d'environnement depuis .env.
+    load_dotenv()
+
+    # Crée une session AWS avec les clés d'accès et la région définies dans les variables d'environnement.
+    aws_session = boto3.Session(
+        aws_access_key_id=os.getenv("ACCESS_KEY"),        # Récupère l'ID de clé d'accès depuis les variables d'environnement.
+        aws_secret_access_key=os.getenv("SECRET_KEY"),    # Récupère la clé d'accès secrète depuis les variables d'environnement.
+        region_name="us-east-1"                           # Spécifie la région AWS à utiliser.
+    )
+    
+    # Retourne l'objet session créé.
+    return aws_session
+
 
 
 if __name__ == "__main__":
-    True
+    TEST_VIDEO_FILE = "./assets/tuto_maquillage.mp4"
+    TEST_IMAGE_FILE = "./assets/selfie_with_johnny-depp.png"
+
+    video = check_filetype(TEST_VIDEO_FILE)
+    image = check_filetype(TEST_IMAGE_FILE)
+
+    print(video)
+    print(image)
+
+    TEST_VIDEO_FILE = "./assets/tuto_jeux-video.mp4"
+
+    frame_video = extract_frame_video(TEST_VIDEO_FILE,0)
+
+
+    imgplot = plt.imshow(frame_video)
+    plt.show()
